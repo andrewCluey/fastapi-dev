8d16ed3baae20037e34dc1674e90ec4ed8daac14
46061505@ad.mmu.ac.uk <a.clure@mmu.ac.uk>
2024-01-17 13:09:11 +0000

    initial

--- function_app.py
+++ function_app.py
@@ -1,5 +1,16 @@
+
 import azure.functions as func
 
-from WrapperFunction import app as fastapi_app
+from api.fastapi_app import create_app
 
-app = func.AsgiFunctionApp(app=fastapi_app, http_auth_level=func.AuthLevel.ANONYMOUS)
+fastapi_app = create_app()
+
+app = func.AsgiFunctionApp(app=fastapi_app, http_auth_level=func.AuthLevel.FUNCTION)
+
+
+
+#import azure.functions as func
+
+#from WrapperFunction import app as fastapi_app
+
+#app = func.AsgiFunctionApp(app=fastapi_app, http_auth_level=func.AuthLevel.ANONYMOUS)
